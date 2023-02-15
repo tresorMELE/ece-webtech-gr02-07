0e06f78d3091b0fb41d3b7e1d30f248a0244f45c
TomTallec <tomice2001@gmail.com>
2023-02-15 09:06:26 +0100

    feat : html content

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 // Import a module
 const http = require('http')
 
-// Declare an http server
-http.createServer(function (req, res) {
+// Define a string constant concatenating strings
+const content = '<!DOCTYPE html>' +
+    '<html lang="fr">' +
+    '    <head>' +
+    '        <meta charset="utf-8" />' +
+    '        <title>ECE AST</title>' +
+    '    </head>' +
+    '    <body>' +
+    '       <p>Hello World!</p>' +
+    '    </body>' +
+    '</html>'
 
-    // Write a response header
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+const serverHandle = function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(content);
+    res.end();
+}
 
-    // Write a response content
-    res.end('Hello World\n');
-
-// Start the server
-}).listen(8080)
+const server = http.createServer(serverHandle);
+server.listen(8080)
