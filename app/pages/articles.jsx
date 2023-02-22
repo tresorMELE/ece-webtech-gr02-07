f354d1804fbd343f04c62f241a6d6579d9ba79e7
Tresor MELE <57411012+tresorMELE@users.noreply.github.com>
2023-02-22 12:19:15 +0100

    feat: ajout des articles

--- app/pages/articles.jsx
+++ app/pages/articles.jsx
@@ -1,3 +1,29 @@
+const articles = [
+      {
+        id: 1,
+        title: 'My article',
+        content: 'Content of the article.',
+
+      },
+      {
+        id: 2,
+        title: 'My article 2',
+        content: 'Content of the article 2',
+
+      },
+
+    ];
+
 export default function ArticlesPage(){
-    return <div>Welcome to the articles page!</div>
-}
+    return(
+    <div>
+        <h1>Welcome to the articles page!</h1>
+        <ul>
+        {articles.map((article) =>{
+                <li key={article.id}>
+                    <p>{article.title} {article.content}</p>
+                </li>
+        })}
+        </ul>
+    </div>
+)};
